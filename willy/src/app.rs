4eeda516dddc4dd91de29de4edd07973b33450ee
Will <wmedrano@users.noreply.github.com>
2023-12-22 22:34:07 -0800

    Create basic editor. (#1)

---
+++ willy/src/app.rs
@@ -0,0 +1,228 @@
+use anyhow::{bail, Result};
+use crossterm::event;
+use ratatui::{prelude::Backend, Terminal};
+
+use crate::{buffer::Buffer, widgets::buffer::BufferWidget};
+
+/// Runs the `willy` application.
+pub struct App<B: Backend> {
+    terminal: Terminal<B>,
+    state: State,
+}
+
+/// Stores the state of the `willy` application.
+pub struct State {
+    buffer: Buffer,
+    should_exit: bool,
+}
+
+#[cfg(test)]
+impl App<ratatui::backend::TestBackend> {
+    /// Create a new app with a dummy backend.
+    pub fn new_dummy(width: u16, height: u16) -> Result<App<ratatui::backend::TestBackend>> {
+        Self::new(ratatui::backend::TestBackend::new(width, height))
+    }
+
+    /// Get the underlying buffer for the test backend.
+    pub fn buffer(&self) -> &ratatui::prelude::Buffer {
+        self.terminal.backend().buffer()
+    }
+}
+
+impl<B: Backend> App<B> {
+    /// Create a new `App`.
+    pub fn new(backend: B) -> Result<App<B>> {
+        let mut terminal = Terminal::new(backend)?;
+        terminal.clear()?;
+        Ok(App {
+            terminal,
+            state: State::default(),
+        })
+    }
+
+    /// Render the current state of the `App`.
+    pub fn render(&mut self) -> Result<()> {
+        if self.state.should_exit {
+            bail!("Exit requested.");
+        }
+        self.terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(BufferWidget::new(&self.state.buffer), area);
+        })?;
+        Ok(())
+    }
+
+    /// Handle all events and update the `App` state accordingly.
+    pub fn handle_events(
+        &mut self,
+        events: impl IntoIterator<Item = Result<event::Event>>,
+    ) -> Result<()> {
+        for event_or_err in events.into_iter() {
+            self.handle_event(event_or_err?);
+        }
+        Ok(())
+    }
+
+    fn handle_event(&mut self, e: event::Event) {
+        match e {
+            event::Event::Key(event::KeyEvent {
+                code: event::KeyCode::Char('c'),
+                modifiers: event::KeyModifiers::CONTROL,
+                kind: event::KeyEventKind::Press,
+                ..
+            }) => self.state.should_exit = true,
+            event::Event::Key(event::KeyEvent {
+                code: event::KeyCode::Enter,
+                modifiers: event::KeyModifiers::NONE,
+                kind: event::KeyEventKind::Press,
+                ..
+            }) => self.state.buffer.push_char('\n'),
+            event::Event::Key(event::KeyEvent {
+                code: event::KeyCode::Backspace,
+                modifiers: event::KeyModifiers::NONE,
+                kind: event::KeyEventKind::Press,
+                ..
+            }) => {
+                self.state.buffer.pop_char();
+            }
+            event::Event::Key(event::KeyEvent {
+                code: event::KeyCode::Char(ch),
+                modifiers: event::KeyModifiers::NONE,
+                kind: event::KeyEventKind::Press,
+                ..
+            }) => self.state.buffer.push_char(ch),
+            event::Event::Key(event::KeyEvent {
+                code: event::KeyCode::Char(ch),
+                modifiers: event::KeyModifiers::SHIFT,
+                kind: event::KeyEventKind::Press,
+                ..
+            }) => self.state.buffer.push_chars(ch.to_uppercase()),
+            _ => (),
+        }
+    }
+}
+
+impl Default for State {
+    fn default() -> State {
+        State {
+            buffer: Buffer::new_scratch(),
+            should_exit: false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ratatui::assert_buffer_eq;
+
+    fn make_key_press(ch: char) -> event::Event {
+        assert!(ch.is_ascii()); // Only ascii us supported for testing.
+        event::Event::Key(event::KeyEvent {
+            code: event::KeyCode::Char(ch.to_ascii_lowercase()),
+            modifiers: if ch.is_lowercase() {
+                event::KeyModifiers::NONE
+            } else {
+                event::KeyModifiers::SHIFT
+            },
+            kind: event::KeyEventKind::Press,
+            state: event::KeyEventState::empty(),
+        })
+    }
+
+    use super::*;
+
+    #[test]
+    fn ctr_c_exits_app() {
+        let mut app = App::new_dummy(80, 20).unwrap();
+
+        app.handle_events(std::iter::once(Ok(make_key_press('c'))))
+            .unwrap();
+        assert!(app.render().is_ok());
+
+        app.handle_events(std::iter::once(Ok(event::Event::Key(event::KeyEvent {
+            code: event::KeyCode::Char('c'),
+            modifiers: event::KeyModifiers::CONTROL,
+            kind: event::KeyEventKind::Press,
+            state: event::KeyEventState::empty(),
+        }))))
+        .unwrap();
+        assert!(app.render().is_err());
+    }
+
+    #[test]
+    fn default_state_has_friendly_message() {
+        let mut app = App::new_dummy(40, 4).unwrap();
+        app.render().unwrap();
+        assert_buffer_eq!(
+            app.buffer(),
+            &ratatui::prelude::Buffer::with_lines(vec![
+                ";; Welcome to Willy!                    ",
+                ";; A Scheme configured text editor.     ",
+                "_                                       ",
+                "                                        ",
+            ])
+        );
+    }
+
+    #[test]
+    fn key_press_inserts_text() {
+        let mut app = App::new_dummy(40, 4).unwrap();
+        app.handle_events([
+            Ok(make_key_press('H')),
+            Ok(make_key_press('e')),
+            Ok(make_key_press('l')),
+            Ok(make_key_press('l')),
+            Ok(make_key_press('o')),
+            Ok(event::Event::Key(event::KeyEvent {
+                code: event::KeyCode::Enter,
+                modifiers: event::KeyModifiers::NONE,
+                kind: event::KeyEventKind::Press,
+                state: event::KeyEventState::empty(),
+            })),
+            Ok(make_key_press('W')),
+            Ok(make_key_press('o')),
+            Ok(make_key_press('r')),
+            Ok(make_key_press('l')),
+            Ok(make_key_press('d')),
+            Ok(make_key_press('!')),
+        ])
+        .unwrap();
+        app.render().unwrap();
+        assert_buffer_eq!(
+            app.buffer(),
+            &ratatui::prelude::Buffer::with_lines(vec![
+                ";; Welcome to Willy!                    ",
+                ";; A Scheme configured text editor.     ",
+                "Hello                                   ",
+                "World!_                                 ",
+            ])
+        );
+    }
+
+    #[test]
+    fn backspace_deletes_text() {
+        let mut app = App::new_dummy(40, 4).unwrap();
+        app.handle_events(
+            std::iter::repeat_with(|| {
+                Ok(event::Event::Key(event::KeyEvent {
+                    code: event::KeyCode::Backspace,
+                    modifiers: event::KeyModifiers::NONE,
+                    kind: event::KeyEventKind::Press,
+                    state: event::KeyEventState::empty(),
+                }))
+            })
+            .take(3),
+        )
+        .unwrap();
+        app.render().unwrap();
+        assert_buffer_eq!(
+            app.buffer(),
+            &ratatui::prelude::Buffer::with_lines(vec![
+                ";; Welcome to Willy!                    ",
+                ";; A Scheme configured text edito_      ",
+                "                                        ",
+                "                                        ",
+            ])
+        );
+    }
+}
---
+++ willy/src/buffer.rs
@@ -0,0 +1,122 @@
+/// Buffer holds editable text.
+#[derive(Default)]
+pub struct Buffer {
+    lines: Vec<String>,
+}
+
+impl Buffer {
+    /// Create a new empty `Buffer`.
+    pub fn new() -> Buffer {
+        Buffer::default()
+    }
+
+    /// Create a new buffer from text.
+    pub fn with_text(text: &str) -> Buffer {
+        Buffer {
+            lines: text.split('\n').map(str::to_string).collect(),
+        }
+    }
+
+    /// Create a new scratch buffer. The buffer contains a friendly message.
+    pub fn new_scratch() -> Buffer {
+        Buffer {
+            lines: vec![
+                ";; Welcome to Willy!".to_string(),
+                ";; A Scheme configured text editor.".to_string(),
+                "".to_string(),
+            ],
+        }
+    }
+
+    /// Convert the buffer into text
+    pub fn to_text(&self) -> String {
+        self.lines.join("\n")
+    }
+
+    /// Iterate through all lines.
+    pub fn iter_lines(&self) -> impl ExactSizeIterator + Iterator<Item = &str> {
+        self.lines.iter().map(|s| s.as_str())
+    }
+
+    /// Add a new character to the buffer.
+    pub fn push_char(&mut self, ch: char) {
+        if self.lines.is_empty() {
+            self.lines.push(String::new());
+        }
+        if ch == '\n' {
+            self.lines.push(String::new());
+            return;
+        }
+        self.lines.last_mut().unwrap().push(ch);
+    }
+
+    /// Push several characters onto the buffer.
+    pub fn push_chars(&mut self, chs: impl IntoIterator<Item = char>) {
+        for ch in chs.into_iter() {
+            self.push_char(ch);
+        }
+    }
+
+    /// Pop a character from the buffer or `None` if the buffer is empty.
+    pub fn pop_char(&mut self) -> Option<char> {
+        if self.lines.is_empty() {
+            return None;
+        }
+        match self.lines.last_mut().unwrap().pop() {
+            Some(ch) => Some(ch),
+            None => {
+                self.lines.pop();
+                Some('\n')
+            }
+        }
+    }
+}
+
+impl std::fmt::Display for Buffer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for line in self.lines.iter() {
+            writeln!(f, "{line}")?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn push_char_adds_new_char() {
+        let mut buffer = Buffer::new();
+        buffer.push_char('a');
+        assert_eq!(buffer.to_text(), "a");
+    }
+
+    #[test]
+    fn push_chars_adds_new_chars() {
+        let mut buffer = Buffer::new();
+        buffer.push_chars(['a', 'b', 'c', 'd', '\n', 'e', '\n']);
+        assert_eq!(buffer.to_text(), "abcd\ne\n");
+    }
+
+    #[test]
+    fn push_chars_to_buffer_with_text_appends() {
+        let mut buffer = Buffer::with_text("name: ");
+        buffer.push_chars(['w', 'i', 'l', 'l', 'y']);
+        assert_eq!(buffer.to_text(), "name: willy");
+    }
+
+    #[test]
+    fn pop_char_on_empty_buffer_is_none() {
+        let mut buffer = Buffer::new();
+        assert_eq!(buffer.pop_char(), None);
+    }
+
+    #[test]
+    fn pop_char_removes_last_char() {
+        let mut buffer = Buffer::with_text("typoo");
+        assert_eq!(buffer.to_text(), "typoo");
+        assert_eq!(buffer.pop_char(), Some('o'));
+        assert_eq!(buffer.to_text(), "typo");
+    }
+}
