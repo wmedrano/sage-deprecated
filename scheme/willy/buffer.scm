20b325cb9d55746ac50c0e61c42d0f79001eed70
Will Medrano <will.s.medrano@gmail.com>
2023-12-25 22:41:49 -0800

    Cleanup.

--- scheme/willy/buffer.scm
+++ scheme/willy/buffer.scm
@@ -2,7 +2,8 @@
 
 (load-extension "target/release/libwilly.so" "scm_init_willy_buffer_module")
 
-(define scratch-buffer-text ";; Welcome to Willy! A Scheme based text editor.\n")
-
 (define-public (new-scratch-buffer)
+  "Create a new scratch buffer."
   (new-buffer scratch-buffer-text))
+
+(define scratch-buffer-text ";; Welcome to Willy! A Scheme based text editor.\n")
--- scheme/willy/main.scm
+++ scheme/willy/main.scm
@@ -3,41 +3,32 @@
 (use-modules (willy buffer)
 	     (willy tui))
 
-(define log-buffer (new-buffer))
 (define-public (log-message! message)
+  "Log a message to the log buffer."
   (buffer-insert-string log-buffer message)
   (buffer-insert-string log-buffer "\n"))
 
-(define should-run #t)
 (define-public (quit!)
-  (log-message! "I'm quitting!")
+  "Exit/quit out of Willy."
+  (log-message! "Quitting Willy!")
   (set! should-run #f)
   (log-message! (object->string should-run)))
 
+(define-public (run-willy!)
+  "Run the Willy text editor."
+  (set! should-run #t)
+  (set! main-tui (new-tui))
+  (while should-run
+    (draw main-tui main-buffer log-buffer)
+    (handle-all-events!)
+    (usleep 10000))
+  (delete-tui main-tui)
+  (set! main-tui #f))
 
 (define main-buffer (new-scratch-buffer))
+(define log-buffer (new-buffer))
+(define should-run #f)
 (define main-tui #f)
-
-(define (log-event event)
-  (buffer-insert-string log-buffer "Event: ")
-  (buffer-insert-string log-buffer (object->string event))
-  (buffer-insert-string log-buffer "\n"))
-
-(define (handle-event! event)
-  (let ((char   (assoc-ref event #:char))
-	(ctrl?  (assoc-ref event #:ctrl?))
-	(press? (equal? (assoc-ref event #:event-type) 'press)))
-    (buffer-insert-string log-buffer "Debug: ")
-    (buffer-insert-string log-buffer
-			  (object->string (equal? char "c")))
-    (buffer-insert-string log-buffer ", ")
-    (log-event event)
-    (if (and ctrl? (equal? char "c"))
-	(quit!))
-    (if (and press? char (not ctrl?) (not (equal? char backspace-char)))
-	(buffer-insert-string main-buffer char))
-    (if (and press? (equal? char backspace-char))
-	(buffer-pop-char main-buffer))))
 
 (define (handle-all-events!)
   (let handle-single-event ((event (next-event)))
@@ -47,12 +38,20 @@
 	  (handle-single-event (next-event)))
 	'())))
 
-(define-public (run-willy!)
-  (set! should-run #t)
-  (set! main-tui (new-tui))
-  (while should-run
-    (draw main-tui main-buffer log-buffer)
-    (handle-all-events!)
-    (usleep 10000))
-  (delete-tui main-tui)
-  (set! main-tui #f))
+(define (handle-event! event)
+  (let ((char   (assoc-ref event #:char))
+	(ctrl?  (assoc-ref event #:ctrl?))
+	(press? (equal? (assoc-ref event #:event-type) 'press)))
+    (log-event event)
+    (cond
+     ((and ctrl? (equal? char "c"))
+      (quit!))
+     ((and press? (equal? char backspace-char))
+      (buffer-pop-char main-buffer))
+     ((and press? char (not ctrl?))
+      (buffer-insert-string main-buffer char)))))
+
+(define (log-event event)
+  (log-message!
+   (string-concatenate
+    `("Event: " ,(object->string event)))))
