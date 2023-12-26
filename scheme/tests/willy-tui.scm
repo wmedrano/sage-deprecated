806108bbc5616ae18b09c1db5f63ba1dedf85ce5
Will Medrano <will.s.medrano@gmail.com>
2023-12-26 11:07:49 -0800

    Add test for multiple buffers.

--- scheme/tests/willy-tui.scm
+++ scheme/tests/willy-tui.scm
@@ -72,4 +72,38 @@
     (tui-draw tui (list (new-buffer "example text\n  pass: true ")))
     (tui-state-for-test tui)))
 
+(test-equal "can render multiple buffers"
+  (string-concatenate
+   '(
+     "buffer a_                                                                       \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "buffer b_                                                                       \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "buffer c_                                                                       \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "                                                                                \n"
+     "Looking at a buffer in Willy!                                                   "))
+  (let ((tui   (new-tui 'test))
+	(buf-a (new-buffer "buffer a"))
+	(buf-b (new-buffer "buffer b"))
+	(buf-c (new-buffer "buffer c")))
+    (tui-draw tui (list buf-a buf-b buf-c))
+    (tui-state-for-test tui)))
+
 (test-end %test-suite-name)
