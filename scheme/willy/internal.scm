e7a31c9ec1f3947b1d02b7d89e4235b8d158cd49
Will Medrano <will.s.medrano@gmail.com>
2023-12-27 13:15:17 -0800

    Fix compile warnings.

--- scheme/willy/internal.scm
+++ scheme/willy/internal.scm
@@ -1,1 +1,10 @@
-(define-module (willy internal))
+(define-module (willy internal)
+  #:export (
+	    --buffer-content-insert-string
+	    --buffer-content-pop-char
+	    --buffer-content-to-string
+	    --delete-tui --tui-draw --tui-size --tui-state-for-test
+	    --make-buffer-content
+	    --make-tui
+	    --next-event-from-terminal
+	    ))
