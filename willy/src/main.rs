9629fe18baf0c6086da6bf18b4498014efa0fa4b
Will Medrano <will.s.medrano@gmail.com>
2023-12-26 17:22:09 -0800

    Format.

--- willy/src/main.rs
+++ willy/src/main.rs
@@ -6,12 +6,12 @@ mod tui;
 fn main() {
     unsafe {
         flashkick::boot_guile(std::env::args(), || {
-            let result = catch_unwind(|| {
+            catch_unwind(|| {
                 tui::scm::scm_init_willy_internal_tui_module();
                 buffer::scm::scm_init_willy_internal_buffer_module();
                 flashkick::shell(std::env::args());
-            });
-            result.unwrap();
+            })
         })
+        .unwrap();
     }
 }
