c8fb38e463bde5600419c046d90ccd8347dc9c60
Will Medrano <will.s.medrano@gmail.com>
2023-12-30 10:40:56 -0800

    Uncomment out test.

--- scheme/tests/window-test.scm
+++ scheme/tests/window-test.scm
@@ -5,22 +5,22 @@
 (define %test-suite-name "window")
 (test-begin %test-suite-name)
 
-;; (test-equal "make-window sets fields"
-;;             '(
-;;               (buffer   . "my buffer")
-;;               (features . ((a . 1) (b . "value")  (c . #t)))
-;;               (x        . 1)
-;;               (y        . 2)
-;;               (width    . 3)
-;;               (height   . 4))
-;;             (make-window #:buffer "my buffer"
-;;                          #:features '((a . 1)
-;;                                       (b . "value")
-;;                                       (c . #t))
-;;                          #:x 1
-;;                          #:y 2
-;;                          #:width 3
-;;                          #:height 4))
+(test-equal "make-window sets fields"
+            '(
+              (buffer   . "my buffer")
+              (features . ((a . 1) (b . "value")  (c . #t)))
+              (x        . 1)
+              (y        . 2)
+              (width    . 3)
+              (height   . 4))
+            (make-window #:buffer "my buffer"
+                         #:features '((a . 1)
+                                      (b . "value")
+                                      (c . #t))
+                         #:x 1
+                         #:y 2
+                         #:width 3
+                         #:height 4))
 
 (test-equal "can get value for specific feature"
             "value"
